1ee9eb29e685c7de02fcf3991d036a6ac2d8ce67
LoganMoffett <8loganm@gmail.com>
2021-02-13 09:05:58 -0700

    template

---
+++ mastermind_template/mastermind/game/board.py
@@ -0,0 +1,20 @@
+class Board:
+    """
+    creates the board
+    """
+    def __init__(self):
+        self.board = []
+        self._checked_guess = []
+
+    def create_board(self):
+        return board
+
+    def check_guess(self, board, move):
+        self._checked_guess
+        self.board = move
+
+    def to_string(self):
+        """
+        prepares everything to be printed
+        takes _checked_guess and board and returns a string forgatted to be printed
+        """
